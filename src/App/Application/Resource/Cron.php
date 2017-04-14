db60622a44791d57c9c44f9077ea7bdfda758617
Anh Nguyen <anhnt0212@gmail.com>
2017-04-14 16:12:29 +0700

    first commit

---
+++ src/App/Application/Resource/Cron.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * anhnt0212@gmail.com Company Limited
+ *
+ * Object Role Modeling (ORM) is a powerful method for designing and querying
+ * database models at the conceptual level, where the application is described in
+ * terms easily understood by non-technical users. In practice, ORM data models
+ * often capture more business rules, and are easier to validate and evolve than
+ * data models in other approaches.
+ *
+ * anhnt0212@gmail.com is a software development company
+ * specializing in Web Application, Mobile Application and Multimedia. anhnt0212@gmail.com ZFCMS's combination of experience
+ * and specialization on Internet technologies extends our customers' competitive
+ * advantage and helps them maximize their return on investment. We aim to realize
+ * your company's goals and vision though ongoing communication and our commitment
+ * to quality.
+ *
+ * @category 	App
+ * @package 	App.Platform
+ * @copyright 	Copyright (c) 2010-2014 anhnt0212@gmail.com.
+ * @license 	http://xemtintucnhanh.top
+ * @version 	App version 1.0.0
+ * @author 	anhnt0212@gmail.com <anhnt0212@gmail.com>
+ * @implement 	All anhnt0212@gmail.com members
+ */
+class App_Application_Resource_Cron extends Zend_Application_Resource_ResourceAbstract {
+	
+	public function init() {
+		$options = $this->getOptions ();
+		if (array_key_exists ( 'pluginPaths', $options )) {
+			$cron = new App_Service_Cron ( $options ['pluginPaths'] );
+		} else {
+			$cron = new App_Service_Cron ( array ('App_Service_Cron_Adapter' => realpath ( __DIR__.'/../../Service/Cron/Adapter/') ) );
+		}
+		
+		if (array_key_exists ( 'actions', $options )) {
+			foreach ( $options ['actions'] as $name => $args ) {
+				$cron->addAction ( $name, $args );
+			}
+		}
+		
+		return $cron;
+	}
+}
---
+++ src/App/Application/Resource/Multidb.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @desc  Override multidb resource
+ * @author Anh Nguyen <anhnt0212@gmail.com>
+ */
+class App_Application_Resource_Multidb extends Zend_Application_Resource_Multidb {
+}
